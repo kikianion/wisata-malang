9922683c1e083891455c3d4bfa224ad729a0610e
nuha <nuha@harap.id>
2021-07-03 20:02:05 +0700

    fix proc buku tamu

--- app/include/proc_buku_tamu.php
+++ app/include/proc_buku_tamu.php
@@ -1,13 +1,15 @@
-<?
+<?php
 ob_start();
 include_once "lib/koneksi.php";
 
-$nama=$_POST['nama'];
-$email=$_POST['email'];
-$pesan=$_POST['pesan'];
-$tgl=date("Ymd");
-$sql="insert into buku_tamu(nama,email,pesan,date) values('$nama','$email','$pesan','$tgl')";
+$nama = $_POST['nama'];
+$email = $_POST['email'];
+$pesan = $_POST['pesan'];
+$tgl = date("Ymd");
+$sql = "insert into buku_tamu(nama,email,pesan,date) values('$nama','$email','$pesan','$tgl')";
 mysql_query($sql);
-header('location:index.php?module=buku_tamu');
-
+//header('location:index.php?module=buku_tamu');
 ?>
+<script>
+  window.location.href = "index.php?module=bukutamu";
+</script>
